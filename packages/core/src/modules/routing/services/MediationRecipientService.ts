3bf9e2d82c34d0f961a0657f8840fef2d39eee68
Timo Glastra <timo@animo.id>
2022-07-07 11:19:33 +0200

    feat: add agent context provider (#921)
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/modules/routing/services/MediationRecipientService.ts
+++ packages/core/src/modules/routing/services/MediationRecipientService.ts
@@ -320,6 +320,7 @@ export class MediationRecipientService {
         type: AgentEventTypes.AgentMessageReceived,
         payload: {
           message: attachment.getDataAsJson<EncryptedMessage>(),
+          contextCorrelationId: messageContext.agentContext.contextCorrelationId,
         },
       })
     }
