b7b7ae28a586cc986717ddad1975d9bdb09c8503
Timo Glastra <timo@animo.id>
2022-07-21 17:05:51 +0200

    feat(dids): add did registrar (#953)
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/modules/routing/services/__tests__/MediationRecipientService.test.ts
+++ packages/core/src/modules/routing/services/__tests__/MediationRecipientService.test.ts
@@ -16,6 +16,7 @@ import { DidExchangeState } from '../../../connections'
 import { ConnectionRepository } from '../../../connections/repository/ConnectionRepository'
 import { ConnectionService } from '../../../connections/services/ConnectionService'
 import { DidRepository } from '../../../dids/repository/DidRepository'
+import { DidRegistrarService } from '../../../dids/services/DidRegistrarService'
 import { RecipientModuleConfig } from '../../RecipientModuleConfig'
 import {
   DeliveryRequestMessage,
@@ -44,6 +45,9 @@ const EventEmitterMock = EventEmitter as jest.Mock<EventEmitter>
 jest.mock('../../../../agent/MessageSender')
 const MessageSenderMock = MessageSender as jest.Mock<MessageSender>
 
+jest.mock('../../../dids/services/DidRegistrarService')
+const DidRegistrarServiceMock = DidRegistrarService as jest.Mock<DidRegistrarService>
+
 const connectionImageUrl = 'https://example.com/image.png'
 
 const mockConnection = getMockConnection({
@@ -59,6 +63,7 @@ describe('MediationRecipientService', () => {
   let wallet: Wallet
   let mediationRepository: MediationRepository
   let didRepository: DidRepository
+  let didRegistrarService: DidRegistrarService
   let eventEmitter: EventEmitter
   let connectionService: ConnectionService
   let connectionRepository: ConnectionRepository
@@ -84,7 +89,14 @@ describe('MediationRecipientService', () => {
     eventEmitter = new EventEmitterMock()
     connectionRepository = new ConnectionRepositoryMock()
     didRepository = new DidRepositoryMock()
-    connectionService = new ConnectionService(config.logger, connectionRepository, didRepository, eventEmitter)
+    didRegistrarService = new DidRegistrarServiceMock()
+    connectionService = new ConnectionService(
+      config.logger,
+      connectionRepository,
+      didRepository,
+      didRegistrarService,
+      eventEmitter
+    )
     mediationRepository = new MediationRepositoryMock()
     messageSender = new MessageSenderMock()
 
